6a46c060502d49083baad049c27c92330d6f252a
Leandro de Amorim <androrim@gmail.com>
2017-11-04 21:01:39 -0200

    Author simple add action

--- src/Stormtech/AuthorsBundle/Controller/AuthorsController.php
+++ src/Stormtech/AuthorsBundle/Controller/AuthorsController.php
@@ -4,14 +4,34 @@ namespace Stormtech\AuthorsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Stormtech\AuthorsBundle\Entity\Author;
 
 class AuthorsController extends Controller
 {
+
     /**
      * @Route("/authors")
      */
     public function indexAction()
     {
         return $this->render('AuthorsBundle:Authors:index.html.twig');
+    }
+
+    /**
+     * @Route("/authors/add")
+     */
+    public function addAction(Request $request)
+    {
+        if ($request->getMethod() === 'POST') {
+            $author = new Author();
+            $author->setName($request->get('name'));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($author);
+            $em->flush();
+        }
+
+        return $this->render('AuthorsBundle:Authors:add.html.twig');
     }
 }
