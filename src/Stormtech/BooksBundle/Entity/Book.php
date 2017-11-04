a93fcf3efdc261bade7cf2fb6410d0f1366e540b
Leandro de Amorim <androrim@gmail.com>
2017-11-04 17:37:19 -0200

    Updated BooksBundle

---
+++ src/Stormtech/BooksBundle/Entity/Book.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Stormtech\BooksBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Book
+ *
+ * @ORM\Table(name="book")
+ * @ORM\Entity(repositoryClass="Stormtech\BooksBundle\Repository\BookRepository")
+ */
+class Book
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="edition_year", type="string", length=4)
+     */
+    private $editionYear;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Book
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return Book
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set editionYear
+     *
+     * @param string $editionYear
+     *
+     * @return Book
+     */
+    public function setEditionYear($editionYear)
+    {
+        $this->editionYear = $editionYear;
+
+        return $this;
+    }
+
+    /**
+     * Get editionYear
+     *
+     * @return string
+     */
+    public function getEditionYear()
+    {
+        return $this->editionYear;
+    }
+}
+
