33c5383ac29ba09b1d05c8c24e62654762298352
Leandro de Amorim <androrim@gmail.com>
2017-11-05 12:53:57 -0200

    Creating book

--- src/Stormtech/BooksBundle/Controller/BooksController.php
+++ src/Stormtech/BooksBundle/Controller/BooksController.php
@@ -4,6 +4,9 @@ namespace Stormtech\BooksBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Stormtech\BooksBundle\Entity\Book;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  *
@@ -17,5 +20,38 @@ class BooksController extends Controller
     public function indexAction()
     {
         return $this->render('BooksBundle:Books:index.html.twig');
+    }
+
+    /**
+     * @Route("/add", name="books_add")
+     */
+    public function addAction(Request $request)
+    {
+        $authorRepository = $this->getDoctrine()
+            ->getRepository('AuthorsBundle:Author');
+
+        $book    = new Book();
+        $authors = $authorRepository->findAll();
+
+        if ($request->getMethod() === 'POST') {
+            $_authors = $authorRepository->findByIds((array) $request->get('authors'));
+
+            var_dump($_authors); die;
+
+            $book->setTitle($request->get('title'));
+            $book->setEditionYear($request->get('edition_year'));
+            $book->setAuthors(new ArrayCollection($_authors));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($book);
+            $em->flush();
+        }
+
+        return $this->render('BooksBundle:Books:addedit.html.twig',
+                [
+                'book' => $book,
+                'selecteds' => [],
+                'authors' => $authors,
+        ]);
     }
 }
